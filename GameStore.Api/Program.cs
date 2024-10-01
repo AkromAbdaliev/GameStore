3b5a50fdc8ae7c5684d799950d2bd024cfe79b86
AkromAbdaliev <jacquesalaiyxs@gmail.com>
2024-10-01 10:28:08 +0300

    GameDto and list added

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,18 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
+
+List<GameDto> games = new()
+{
+    new GameDto(1, "The Last Adventure", "Action", 49.99m, new DateOnly(2021, 5, 17)),
+    new GameDto(2, "Mystery Quest", "Puzzle", 29.99m, new DateOnly(2022, 3, 9)),
+    new GameDto(3, "Space Battle", "Sci-Fi", 59.99m, new DateOnly(2020, 11, 25))
+};
+
+app.MapGet("games", () => games);
 
 app.MapGet("/", () => "Hello World!");
 
