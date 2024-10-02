73c96bdb6ed376545497a7f34b1d199cce388c45
AkromAbdaliev <jacquesalaiyxs@gmail.com>
2024-10-02 17:53:08 +0300

    genres endpoints and http files are added

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d310bd0b2219cbf68e3385affea7ad541d4e90a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34c3d235f4c4d791b9961ad3e56c2f93fec2030")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
