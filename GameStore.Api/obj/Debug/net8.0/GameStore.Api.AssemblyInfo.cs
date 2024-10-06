a9cf4b550c4be972f9533a1bcaa8494693cc32bb
AkromAbdaliev <jacquesalaiyxs@gmail.com>
2024-10-06 16:38:49 +0300

    final commit

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c96bdb6ed376545497a7f34b1d199cce388c45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d8088ffaa5d2582e9a46257be501087f5369cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
